f24c7bf54e1215c035f4c5081b6cf71a8d79d2b8
Auh3b <ngaiyayec@yahoo.com>
2021-02-16 19:01:49 +0200

    add more details

--- app.js
+++ app.js
@@ -31,10 +31,63 @@ getData("testing_sites.json")
       var eLat = nearest[0].lat;
       var eLong = nearest[0].lon;
 
-      L.mapquest.directions().route({
-        start: [sLat, sLong],
-        end: [eLat, eLong],
+      L.mapquest.directions().route(
+        {
+          start: [sLat, sLong],
+          end: [eLat, eLong],
+        },
+        response
+      );
+
+      let destination = `<div id="destination">
+      <i id="close" class="fas fa-window-close"></i>
+      <p>Name : ${nearest[0].layer.feature.properties.NAME} ${nearest[0].layer.feature.properties.TYPE}</p>
+      <p>Address: ${nearest[0].layer.feature.properties.ADDRESS}</p>
+      <p>Estimated Distance: <span id="distance" ></span> KM</p>
+      <p>Estimated Travel Time: <span id="time"></span></p>
+      </div>`;
+
+      document.getElementById("dest").innerHTML = destination;
+
+      let close = document.getElementById("close");
+
+      close.addEventListener("click", () => {
+        document.getElementById("map").style.zIndex = 1;
+        document.getElementById("dest").style.transform = "translateY(0px)";
       });
+    }
+    let travelTime, calDistance;
+    function response(err, route) {
+      travelTime = route.route.formattedTime;
+      calDistance = route.route.distance;
+      document.getElementById("time").textContent = `${travelTime}`;
+      document.getElementById("distance").textContent = `${calDistance}`;
+      var DirectionsLayerWithCustomMarkers = L.mapquest.DirectionsLayer.extend({
+        createStartMarker: function (location, stopNumber) {
+          return L.marker(location.latLng, {
+            iconOptions: {
+              size: "sm",
+              primaryColor: "#1fc715",
+              secondaryColor: "#1fc715",
+              symbol: "A",
+            },
+          }).bindPopup("Your Location");
+        },
+
+        createEndMarker: function (location, stopNumber) {
+          return L.marker(location.latLng, {}).on("click", showDetails);
+        },
+      });
+
+      var directionsLayer = new DirectionsLayerWithCustomMarkers({
+        directionsResponse: route,
+      }).addTo(map);
+    }
+
+    // show
+    function showDetails() {
+      document.getElementById("map").style.zIndex = -1;
+      document.getElementById("dest").style.transform = "translateY(-330px)";
     }
 
     function error(err) {
